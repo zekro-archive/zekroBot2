f25e7fda2a14e794f6fc08e252d86e56a9ab17cd
Ringo Hoffmann <rh@digitronic.net>
2018-01-22 10:28:03 +0100

    edited some stuff in command register

---
+++ src/commands/perms.coffee
@@ -0,0 +1,8 @@
+Main = require '../main'
+client = Main.client
+Discord = require 'discord.js'
+Embeds = require '../util/embeds'
+
+
+exports.ex = (msg, args) ->
+    
--- src/core/cmdhandler.js
+++ src/core/cmdhandler.js
@@ -2,6 +2,19 @@ const Main = require('../main')
 const { CmdParser } = require('discordjs-cmds')
 
 
+/*
+    TYPES:
+
+    ADMIN:          ADMIN
+    GUILDADMIN:     GUILDADMIN
+    MODERATION:     MODERATION
+    FUN:            FUN
+    SETTING:        SETTING
+    CHAT:           CHAT
+    MISC:           MISC
+
+*/
+
 class CmdHandler {
     constructor(client, prefix) {
         this.cmd = new CmdParser(client, prefix)
@@ -9,8 +22,44 @@ class CmdHandler {
         this.cmd.addType('DEBUG')
 
         this.cmd
-            .register(require('../commands/info').ex, 'info', [], 'Get info about the bot', `\`${prefix}\`info`, this.cmd.type.MISC)
-            .register(require('../commands/test').ex, 'test', [], 'Just for testing purposes', null, 'DEBUG')
+            // INFO COMMAND
+            .register(
+                require('../commands/info').ex, 
+                'info', 
+                ['about'], 
+                'Get info about the bot', 
+                `\`${prefix}info\``, 
+                this.cmd.type.MISC
+            )
+            // TEST COMMAND
+            .register(
+                require('../commands/test').ex, 
+                'test', 
+                [], 
+                'Just for testing purposes', 
+                null, 
+                'DEBUG',
+                5
+            )
+            .register(
+                require('../commands/perms').ex,
+                'perms',
+                ['permroles', 'perm', 'permlvl'],
+                'Set the permission levels for specific roles',
+                `\`${prefix}perms <LVL>, <role1>, <role2>, ...\`\n\`${prefix}perms list\``,
+                this.cmd.type.SETTING,
+                4
+            )
+            // GAME COMMAND
+            .register(
+                require('../commands/game').ex, 
+                'game', 
+                ['playing', 'botmsg'], 
+                'Set messages the bot should show in playing text', 
+                `\`${prefix}game <message 1>, <message 2>, ...\`\n\`${prefix}game reset\``,
+                this.cmd.type.SETTING,
+                4
+            )
 
         this.cmd.event.on('commandFailed', (type, msg, err) => console.log(type, err))
 
