68ef61e70ff503b2ff5d9a5578743cba011d270a
Ringo Hoffmann <riho@live.de>
2018-02-08 16:41:20 +0100

    optimized documentation of util classes

---
+++ src/util/statics.js
@@ -0,0 +1,23 @@
+
+/**
+ * Color statics used for message embeds.
+ */
+exports.COLORS = {
+    main: 0x3498db,
+    error: 0xc0392b,
+    cyan: 0x4DD0E1,
+    green: 0x8BC34A,
+    orange: 0xFFB300
+}
+
+/**
+ * Permission mask for invite link.
+ */
+exports.INVPERMS = 2146958455
+
+/**
+ * Statics directories.
+ */
+exports.DIRS = {
+    cloc: 'https://raw.githubusercontent.com/zekroTJA/zekroBot2/master/CLOC_LOG.md'
+}
