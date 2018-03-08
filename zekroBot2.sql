f1cfc38085bf81bf08cd8769f06c7d6361e430dc
ゼクロ <zekrommaster110@aol.de>
2018-03-08 16:55:21 +0100

    updated mysql db template

--- zekroBot2.sql
+++ zekroBot2.sql
@@ -20,6 +20,8 @@ CREATE TABLE IF NOT EXISTS `guilds` (
   `guild` text NOT NULL,
   `prefix` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `autorole` text NOT NULL,
+  `cmdlogchan` text NOT NULL,
+  `vlogchan` text NOT NULL,
   `autochanprefix` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -34,6 +36,13 @@ CREATE TABLE IF NOT EXISTS `perms` (
   `role` text NOT NULL,
   `lvl` text NOT NULL,
   `guild` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `r6rerolls`;
+CREATE TABLE IF NOT EXISTS `r6rerolls` (
+  `guild` text NOT NULL,
+  `member` text NOT NULL,
+  `time` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `reports`;
@@ -55,4 +64,11 @@ DROP TABLE IF EXISTS `votes`;
 CREATE TABLE IF NOT EXISTS `votes` (
   `membid` text NOT NULL,
   `data` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `xp`;
+CREATE TABLE IF NOT EXISTS `xp` (
+  `user` text NOT NULL,
+  `xp` bigint(20) NOT NULL DEFAULT '0',
+  `guild` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
