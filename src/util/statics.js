0d56b76ef796197a8506fdea4bc77b70e32f8d37
Ringo Hoffmann <riho@live.de>
2018-03-23 13:34:06 +0100

    updated music command and colors in statics

--- src/util/statics.js
+++ src/util/statics.js
@@ -4,9 +4,25 @@
 exports.COLORS = {
     main: 0x3498db,
     error: 0xc0392b,
-    cyan: 0x4DD0E1,
-    green: 0x8BC34A,
-    orange: 0xFFB300
+    "red": 0xf44336,
+    "pink": 0xE91E63,
+    "purple": 0x9C27B0,
+    "deep_purple": 0x673AB7,
+    "indigo": 0x3F51B5,
+    "blue": 0x2196F3,
+    "light_blue": 0x03A9F4,
+    "cyan": 0x00BCD4,
+    "teal": 0x009688,
+    "green": 0x4CAF50,
+    "light_green": 0x8BC34A,
+    "lime": 0xCDDC39,
+    "yellow": 0xFFEB3B,
+    "amber": 0xFFC107,
+    "orange": 0xFF9800,
+    "deep_orange": 0xFF5722,
+    "brown": 0x795548,
+    "grey": 0x9E9E9E,
+    "blue_grey": 0x607D8B
 }
 
 /**
