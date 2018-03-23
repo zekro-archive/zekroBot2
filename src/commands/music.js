678dd45f8b4d5ad4421b98ad8c64954bf380ae16
Ringo Hoffmann <riho@live.de>
2018-03-23 16:45:25 +0100

    added playlist function to music command

--- src/commands/music.js
+++ src/commands/music.js
@@ -6,9 +6,12 @@ const Discord = require('discord.js')
 const Logger = require('../util/logger')
 const Statics = require('../util/statics')
 const YTDL = require('ytdl-core')
-const EventEmitter = require('events');
+const EventEmitter = require('events')
+const DL = require('../util/dl')
 
 const TEST_URL = 'https://www.youtube.com/watch?v=H31cS2pDAys'
+const PL_TEST_URL = "https://www.youtube.com/playlist?list=PLthqW7GiLEwKYWTl016UnYisBPHUfhmWm"
+const PL_TEST_URL_2 = "https://www.youtube.com/watch?v=7QO2Fn-4i8E&index=1&list=PLthqW7GiLEwKYWTl016UnYisBPHUfhmWm&t=0s"
 
 var server = {}
 
@@ -24,6 +27,8 @@ var server = {}
       was executed
 
     - create that other commands
+
+    https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playlist_id}&key=${key}
 
     PLAYLIST HANDLING → https://stackoverflow.com/questions/41827728/how-to-get-video-ids-from-an-youtube-playlist
 
@@ -63,6 +68,9 @@ function track_end_handler(guild, conn) {
         server[guild.id].dispatcher = play(guild, conn, server[guild.id].queue[0])
     }
     else {
+        if (server[guild.id].tchan.topic.startsWith(':musical_note:')) {
+            server[guild.id].tchan.setTopic(' ')
+        }
         server[guild.id] = null
         conn.disconnect()
     }
@@ -70,12 +78,16 @@ function track_end_handler(guild, conn) {
 
 function track_start_handler(guild, track) {
     let next = server[guild.id].queue[1]
+    let chan = server[guild.id].tchan
     let emb = new Discord.RichEmbed()
         .setColor(Statics.COLORS.cyan)
         .setTitle('NOW PLAYING')
         .addField('Current Track', `:arrow_forward:  \`[${track.info.length_f}]\` **${track.info.title}**`)
         .addField('Next Track', next ? `:track_next:  \`[${next.info.length_f}]\` **${next.info.title}**` : ':track_next:  *End of queue*')
-    server[guild.id].tchan.send('', emb)
+    chan.send('', emb)
+    
+    if (!chan.topic || chan.topic == ' ')
+        chan.setTopic(`:musical_note:   [${track.info.length_f}] ${track.info.title}`)
 }
 
 
@@ -93,14 +105,20 @@ function play(guild, conn, track) {
         })
 }
 
-function queue(member, vc, tchan, url) {
+function queue(member, vc, tchan, url, notsendmsg) {
 
     let conn = member.guild.voiceConnection
     let guild = member.guild
 
+    try {
+        var track = new Track(member, url)
+    }
+    catch (e) {
+        return
+    }
+
     if (!server[guild.id])
         server[guild.id] = {queue: []}
-    let track = new Track(member, url)
     server[guild.id].queue.push(track)
     server[guild.id].tchan = tchan
 
@@ -118,11 +136,48 @@ function queue(member, vc, tchan, url) {
         }
     }
 
-    track.on('info', (info) => {
-        let emb = new Discord.RichEmbed()
-            .setColor(Statics.COLORS.green)
-            .setDescription('Pushed new tack to queue:\n\n' + `\`[${info.length_f}]\` **${info.title}**\n\nTracks in queue: **${server[guild.id].queue.length}**`)
-        tchan.send('', emb)
+    if (!notsendmsg)
+        track.on('info', (info) => {
+            let emb = new Discord.RichEmbed()
+                .setColor(Statics.COLORS.green)
+                .setDescription('Pushed new tack to queue:\n\n' + `\`[${info.length_f}]\` **${info.title}**\n\nTracks in queue: **${server[guild.id].queue.length}**`)
+            tchan.send('', emb)
+        })
+}
+
+function queue_playlist(memb, guild, vc, tchan, url) {
+    return new Promise((resolve, reject) => {
+        if (!Main.config.youtube_api_key || Main.config.youtube_api_key == '') {
+            Embeds.error(tchan, 'No YouTube API key set in bot\'s config.\nYou can only use palylists if the host of this bot added a valid YouTube API link to the config of this bot.', 'MISSING API KEY')
+            reject('No API key')
+            return
+        }
+    
+        let PLAYLIST_ID = url.split('list=')[1].split('&')[0]
+        let API_URL = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${PLAYLIST_ID}&key=${Main.config.youtube_api_key}&maxResults=50`
+        DL.get_ua(API_URL, (err, res) => {
+            if (!err && res) {
+                try {
+                    let data = JSON.parse(res)
+                    let vidurls = data.items.filter(i => i.snippet.thumbnails != null).map(i => i.snippet.resourceId.videoId)
+                    let queued = 0
+                    vidurls.forEach(vurl => {
+                        queued++
+                        queue(memb, vc, tchan, `https://youtube.com/watch?v=${vurl}`, true)
+                    })
+                    let emb = new Discord.RichEmbed()
+                        .setColor(Statics.COLORS.green)
+                        .setDescription(`Pushed **${queued}** tracks to queue.`)
+                    tchan.send('', emb)
+                }
+                catch (e) {
+                    reject(e)
+                }
+            }
+            else {
+                reject(err)
+            }
+        })
     })
 }
 
@@ -155,6 +210,29 @@ function skip(member, guild, tchan, ammount) {
 function stop(member, guild) {
     server[guild.id].queue = []
     server[guild.id].dispatcher.end()
+}
+
+function shuffle(memb, guild, tchan) {
+    var queue = server[guild.id] ? server[guild.id].queue : null
+    if (!queue)
+        Embeds.error(tchan, 'Queue is empty.')
+    else {
+        let first = queue[0]
+        queue.shift()
+        queue = ((array) => {
+            let counter = array.length
+            while (counter > 0) {
+                let index = Math.floor(Math.random() * counter--)
+                let temp = array[counter]
+                array[counter] = array[index]
+                array[index] = temp
+            }
+            return array
+        })(queue)
+        queue.unshift(first)
+        server[guild.id].queue = queue
+        Embeds.default(tchan, ':twisted_rightwards_arrows:  Queue shuffeled', null, Statics.COLORS.deep_orange)
+    }
 }
 
 function displayqueue(guild, tchan) {
@@ -215,7 +293,10 @@ exports.ex = (msg, args) => {
                 Embeds.error(channel, 'Please enter a valid video URL to play!', 'ARGUMENT ERROR')
                 return
             }
-            queue(member, vc, channel, args[1])
+            if (args[1].indexOf('list=') > -1)
+                queue_playlist(member, guild, vc, channel, args[1])
+            else
+                queue(member, vc, channel, args[1])
             break
 
         case 'pause':
@@ -246,6 +327,11 @@ exports.ex = (msg, args) => {
 
         case 'stop':
             stop(member, guild)
+            break
+
+        case 'shuffle':
+        case 'random':
+            shuffle(member, guild, channel)
             break
 
         case 'dc':
